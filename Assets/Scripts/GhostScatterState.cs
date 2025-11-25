294ea831c606c26894a35d0bf7ed31606fb73ba5
Can Akdag <canakdag06@gmail.com>
2025-11-25 13:38:41 +0300

    created GhostScatterState.cs

--- Assets/Scripts/GhostScatterState.cs
+++ Assets/Scripts/GhostScatterState.cs
@@ -2,5 +2,24 @@ using UnityEngine;
 
 public class GhostScatterState : GhostBaseState
 {
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Node node = collision.GetComponent<Node>();
+        if (enabled && !Ghost.FrightenedState.enabled && node != null)
+        {
+            int index = Random.Range(0, node.AvailableDirections.Count);
 
+            if (node.AvailableDirections.Count > 1 && node.AvailableDirections[index] == -Ghost.Movement.Direction)
+            {
+                index++;
+
+                if (index >= node.AvailableDirections.Count)
+                {
+                    index = 0;
+                }
+            }
+
+            Ghost.Movement.SetDirection(node.AvailableDirections[index]);
+        }
+    }
 }
