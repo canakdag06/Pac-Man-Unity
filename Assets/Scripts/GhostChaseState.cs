1f86d2cb53b26e91a82ed63babc8f7928d26fb7e
Can Akdag <canakdag06@gmail.com>
2025-11-25 15:39:51 +0300

    Update GhostChaseState.cs

--- Assets/Scripts/GhostChaseState.cs
+++ Assets/Scripts/GhostChaseState.cs
@@ -2,5 +2,31 @@ using UnityEngine;
 
 public class GhostChaseState : GhostBaseState
 {
+    private void OnDisable()
+    {
+        Ghost.ScatterState.Enable();
+    }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Node node = collision.GetComponent<Node>();
+        if (enabled && !Ghost.FrightenedState.enabled && node != null)
+        {
+            Vector2 direction = Vector2.zero;
+            float minDistance = float.MaxValue;
+
+            foreach (Vector2 dir in node.AvailableDirections)
+            {
+                Vector3 newPosition = transform.position + new Vector3(dir.x, dir.y, 0.0f);
+                float distance = (Ghost.target.position - newPosition).sqrMagnitude;
+
+                if (distance < minDistance)
+                {
+                    direction = dir;
+                    minDistance = distance;
+                }
+            }
+            Ghost.Movement.SetDirection(direction);
+        }
+    }
 }
