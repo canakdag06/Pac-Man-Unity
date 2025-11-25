73542b9f5934d467a8b1add3747058fa8535ad6c
Can Akdag <canakdag06@gmail.com>
2025-11-25 20:18:04 +0300

    updated GhostHomeState

--- Assets/Scripts/GhostHomeState.cs
+++ Assets/Scripts/GhostHomeState.cs
@@ -1,6 +1,51 @@
+using System.Collections;
 using UnityEngine;
 
 public class GhostHomeState : GhostBaseState
 {
+    public Transform inside;
+    public Transform outside;
 
+    private void OnEnable()
+    {
+        StopAllCoroutines();
+    }
+
+    private void OnDisable()
+    {
+        StartCoroutine(ExitHomeAnimation());
+    }
+
+    private IEnumerator ExitHomeAnimation()
+    {
+        Ghost.Movement.SetDirection(Vector2.up, true);
+        Ghost.Movement.rb.bodyType = RigidbodyType2D.Kinematic;
+        Ghost.Movement.enabled = false;
+
+        Vector3 position = transform.position;
+        float duration = 0.5f;
+        float elapsed = 0.0f;
+
+        while(elapsed < duration)
+        {
+            Vector3 newPosition = Vector2.Lerp(position, inside.position, elapsed/duration);
+            elapsed += Time.deltaTime;
+            Ghost.transform.position = newPosition;
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        elapsed = 0.0f;
+        while (elapsed < duration)
+        {
+            Vector3 newPosition = Vector2.Lerp(inside.position, outside.position, elapsed / duration);
+            elapsed += Time.deltaTime;
+            Ghost.transform.position = newPosition;
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        Ghost.Movement.SetDirection(new Vector2(Random.value < 0.5f ? -1.0f : 1.0f, 0.0f), true);
+        Ghost.Movement.rb.bodyType = RigidbodyType2D.Dynamic;
+        Ghost.Movement.enabled = true;
+    }
 }
