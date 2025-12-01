f342809548235ddd845e73c1ea77dc9fc3766794
Can Akdag <canakdag06@gmail.com>
2025-12-01 13:26:39 +0300

    Clyde Chase Behaviour

--- Assets/Scripts/GhostChaseStateClyde.cs
+++ Assets/Scripts/GhostChaseStateClyde.cs
@@ -2,8 +2,28 @@ using UnityEngine;
 
 public class GhostChaseStateClyde : GhostChaseState
 {
+    private const float chaseDistance = 64f; // assigned 8^2 to use sqrMagnitude for optimization
+    private Vector3 corner;
+
+    private void Start()
+    {
+        corner = Ghost.ScatterState.Corner.transform.position;
+    }
+
     protected override Vector3 GetTargetPosition()
     {
-        throw new System.NotImplementedException();
+        Vector3 pacmanPosition = Ghost.target.transform.position;
+        Vector3 clydePosition = transform.position;
+
+        float distance = (clydePosition - pacmanPosition).sqrMagnitude;
+
+        if (distance > chaseDistance)
+        {
+            return pacmanPosition;
+        }
+        else
+        {
+            return corner;
+        }
     }
 }
