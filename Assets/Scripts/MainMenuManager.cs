f178543c372528f8c21c49f25a1c4302583f2581
Can Akdag <canakdag06@gmail.com>
2025-12-06 23:28:49 +0300

    added fadeout for main menu

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,11 +1,40 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
+    [SerializeField] private Image fadeOutImage;
+    public float fadeDuration = 1.0f;
+
     public void StartGame()
     {
-        SceneManager.LoadScene("Game");
+        StartCoroutine(FadeOutAndLoadScene("Game"));
+    }
+
+    private IEnumerator FadeOutAndLoadScene(string sceneName)
+    {
+        Color color = fadeOutImage.color;
+        color.a = 0f;
+        fadeOutImage.color = color;
+
+        fadeOutImage.gameObject.SetActive(true);
+
+        float elapsed = 0f;
+
+        while (elapsed < fadeDuration)
+        {
+            elapsed += Time.deltaTime;
+            float alpha = Mathf.Clamp01(elapsed / fadeDuration);
+
+            color.a = alpha;
+            fadeOutImage.color = color;
+
+            yield return null;
+        }
+
+        SceneManager.LoadScene(sceneName);
     }
 
     public void ExitGame()
